97aa5bf70fb172ad291679b98c7212403132e43d
eshaparenko <gesha8610@gmail.com>
2023-04-09 16:38:47 +0300

    Implemented remove and filter logic

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 let form = document.querySelector('#item-form');
 let itemInput = document.querySelector('#item-input');
 let itemList = document.querySelector('#item-list');
+let clearButton = document.querySelector('.btn-clear');
+let itemFilter = document.querySelector('.filter');
+let filterInput = document.querySelector('.filter')
 
-//Event Listeners on form submit
+
+function checkUI() {
+    let items = itemList.querySelectorAll('li');
+    if (items.length === 0) {
+        clearButton.style.display = 'none';
+        itemFilter.style.display = 'none'
+    } else {
+        clearButton.style.display = 'block';
+        itemFilter.style.display = 'block'
+    }
+}
+ 
+//Event Listener on form submit
 form.addEventListener('submit', (e) => {
     //Create new List Item
-
     if (!itemInput.value.trim()) {
         alert('Please add na item')
         return;
@@ -23,10 +37,49 @@ form.addEventListener('submit', (e) => {
     //Add new List Item to the List
     itemList.appendChild(li);
 
+    checkUI()
     //Clear Item Input
     itemInput.value = ''
 
     e.preventDefault();
+})
+
+//Event Listener on Remove Item
+itemList.addEventListener('click', (e) => {
+    if (e.target.parentElement.classList.contains('remove-item')) {
+        if(confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+            checkUI();
+        }        
+    }
+})
+
+//EL on Remove All 
+clearButton.addEventListener('click', () => {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild)
+    }
+    checkUI();
+})
+
+//EL on page
+document.addEventListener('DOMContentLoaded', checkUI);
+
+
+//EL on filter
+filterInput.addEventListener('input', (e) => {
+    let text = e.target.value.toLowerCase();
+
+    let items = itemList.querySelectorAll('li')
+    items.forEach(item => {
+        let itemName = item.firstChild.textContent.toLowerCase()
+        if (itemName.indexOf(text) != -1) {
+            item.style.display = 'flex'
+        } else {
+            item.style.display = 'none'
+        }
+    })
+
 })
 
 
